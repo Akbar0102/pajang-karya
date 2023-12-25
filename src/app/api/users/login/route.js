ae0bf49e004c96bb2ea73d80c5353bffc9a63e8c
Akbar Hidayatuloh <01akbarh@gmail.com>
2023-12-25 16:05:23 +0700

    chore: payload add role user

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -48,6 +48,7 @@ export async function POST(req) {
       lastName: findUser.lastName,
       username: findUser.username,
       email: findUser.email,
+      role: findUser.role,
     };
 
     // Buat token
