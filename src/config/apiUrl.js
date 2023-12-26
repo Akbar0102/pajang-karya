a92c401716fcefa9f9c400246d0b91302c21fc16
Akbar Hidayatuloh <01akbarh@gmail.com>
2023-12-26 19:20:04 +0700

    chore: set apiUrl for prod and dev

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,2 +1,5 @@
-export const apiUrl = "https://pajang-karya.vercel.app/api";
+export const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://pajang-karya.vercel.app/api"
+    : "http://localhost:3000/api";
 export const imageUrl = "https://ik.imagekit.io/0eh1juqnc/karya";
