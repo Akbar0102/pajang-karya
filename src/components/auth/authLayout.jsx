178eb4c40f1d22414b9dfce6f957cd251276a7a3
Akbar Hidayatuloh <01akbarh@gmail.com>
2024-01-01 21:21:15 +0700

    chore: styling layout auth

--- src/components/auth/authLayout.jsx
+++ src/components/auth/authLayout.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const AuthLayout = ({ children }) => {
   return (
-    <main className="grid grid-cols-2 h-screen">
-      <section className="bg-gradient-to-br from-primary-500 to-primary-700" />
-      <section className="flex justify-center items-center">{children}</section>
+    <main className="grid lg:grid-cols-2 h-screen grid-cols-1">
+      <section className="hidden lg:flex bg-gradient-to-br from-primary-500 to-primary-700" />
+      <section className="flex justify-center items-center space-x-3">{children}</section>
     </main>
   );
 };
