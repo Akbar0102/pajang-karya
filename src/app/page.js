cca72f99b0bd1319e6b82df0b2bf286307d27339
Akbar Hidayatuloh <01akbarh@gmail.com>
2024-01-01 13:06:24 +0700

    chore: update style heading text

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default async function Home({ searchParams }) {
         <section className=" flex flex-col items-center justify-center mt-[130px]">
           <Button radius="full" className=" bg-black-100 px-6 py-4 text-white font-normal text-base">Over 3 million interesting project!</Button>
           <h1 className=" mt-5 text-[52px] text-center text-black-50">Show your code<br/><br/>learning journey</h1>
-          <p className=" mt-5 text-grey">Join developers building real-world project, let expert review your project, and give supporting feedback each others</p>
+          <p className=" mt-5 text-grey sm:w-[512px] sm:text-center">Join developers building real-world project, let expert review your project, and give supporting feedback each others</p>
         </section>
 
         <section className=" flex flex-col items-center mt-[130px] my-[160px]">
