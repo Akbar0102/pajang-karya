aab9ec3c6cffbb377ca9a67cb1fbc8ed4c393438
Akbar Hidayatuloh <01akbarh@gmail.com>
2024-01-07 19:11:50 +0700

    fix: url api domain change

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,5 +1,5 @@
 export const apiUrl =
   process.env.NODE_ENV === "production"
-    ? "https://pajang-karya.vercel.app/api"
+    ? "https://www.karyadev.online/api"
     : "http://localhost:3000/api";
 export const imageUrl = "https://ik.imagekit.io/0eh1juqnc/karya";
